1f1455ac50259f6f921f2525d11d9883875a7b4d
solomon.maeng <solomon.maeng@dramancompany.com>
2023-10-09 20:35:10 +0900

    Initial commit

---
+++ src/test/java/com/rebwon/codekata/CodeKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebwon.codekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
